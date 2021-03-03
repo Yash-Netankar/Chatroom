f393b75b1227bca50fa7c857a7fc671c789c14ce
Yash-Netankar <netankaryash2002@gmail.com>
2021-03-03 10:54:56 +0530

    changed build location

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname + "../build"));
+app.use(express.static(__dirname + "/build"));
 
 
 const users = {}
